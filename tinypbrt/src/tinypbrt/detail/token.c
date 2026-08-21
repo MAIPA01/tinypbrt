957c478c00a43b8b26765832e983cf0b3383790a
MAIPA01 <marcelipatryk@gmail.com>
2026-08-21 14:25:15 +0200

    token

---
+++ tinypbrt/src/tinypbrt/detail/token.c
@@ -0,0 +1,148 @@
+#include <tinypbrt/pch.h>
+
+#include <tinypbrt/detail/token_internal.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	static tpbrt_error_t tpbrt_directive_from_string(const tpbrt_string_t* const directive_str, tpbrt_directive_t* directive) {
+		static const tpbrt_string_t TYPES_STRS[TPBRT_DIRECTIVE_MAX_NUM] = {
+			TPBRT_STRING("Identity"),
+			TPBRT_STRING("Translate"),
+			TPBRT_STRING("Scale"),
+			TPBRT_STRING("Rotate"),
+			TPBRT_STRING("LookAt"),
+			TPBRT_STRING("CoordinateSystem"),
+			TPBRT_STRING("CoordSysTransform"),
+			TPBRT_STRING("Transform"),
+			TPBRT_STRING("ConcatTransform"),
+			TPBRT_STRING("TransformTimes"),
+			TPBRT_STRING("ActiveTransform"),
+
+			TPBRT_STRING("Include"),
+			TPBRT_STRING("Import"),
+
+			TPBRT_STRING("Option"),
+
+			TPBRT_STRING("Camera"),
+			TPBRT_STRING("Sampler"),
+			TPBRT_STRING("ColorSpace"),
+			TPBRT_STRING("Film"),
+			TPBRT_STRING("PixelFilter"),
+			TPBRT_STRING("Integrator"),
+			TPBRT_STRING("Accelerator"),
+
+			TPBRT_STRING("WorldBegin"),
+
+			TPBRT_STRING("AttributeBegin"),
+			TPBRT_STRING("AttributeEnd"),
+			TPBRT_STRING("Attribute"),
+
+			TPBRT_STRING("ReverseOrientation"),
+			TPBRT_STRING("Shape"),
+			TPBRT_STRING("ObjectBegin"),
+			TPBRT_STRING("ObjectEnd"),
+			TPBRT_STRING("ObjectInstance"),
+
+			TPBRT_STRING("LightSource"),
+			TPBRT_STRING("AreaLightSource"),
+
+			TPBRT_STRING("Material"),
+			TPBRT_STRING("MakeNamedMaterial"),
+			TPBRT_STRING("NamedMaterial"),
+
+			TPBRT_STRING("Texture"),
+
+			TPBRT_STRING("MakeNamedMedium"),
+			TPBRT_STRING("MediumInterface"),
+		};
+
+			if (directive_str == TPBRT_NULL || directive_str->chars == TPBRT_NULL || directive == TPBRT_NULL) {
+				return TPBRT_ERROR_INVALID_POINTER;
+			}
+
+			for (tpbrt_directive_t d = 0; d < TPBRT_DIRECTIVE_MAX_NUM; ++d) {
+					if (directive_str->size == TYPES_STRS[d].size &&
+						strncmp(directive_str->chars, TYPES_STRS[d].chars, TYPES_STRS[d].size) == 0) {
+						*directive = d;
+						return TPBRT_ERROR_NONE;
+					}
+			}
+
+		return TPBRT_ERROR_UNKNOWN_DIRECTIVE;
+	}
+
+	tpbrt_error_t tpbrt_create_token(const tpbrt_string_t* const value, tpbrt_token_t** const token) {
+			if (value == TPBRT_NULL || value->chars == TPBRT_NULL || token == TPBRT_NULL) { return TPBRT_ERROR_INVALID_POINTER; }
+
+		*token = malloc(sizeof(tpbrt_token_t));
+			if (*token == TPBRT_NULL) { return TPBRT_ERROR_OUT_OF_MEMORY; }
+
+		(*token)->value			= *value;
+		const tpbrt_error_t err = tpbrt_directive_from_string(value, &(*token)->directive);
+			if (err == TPBRT_ERROR_NONE) {
+				(*token)->type = TPBRT_TOKEN_TYPE_DIRECTIVE;
+				return TPBRT_ERROR_NONE;
+			}
+
+			if (value->size >= 2 && value->chars[0] == '\"' && value->chars[value->size - 1] == '\"') {
+				(*token)->type = TPBRT_TOKEN_TYPE_QUOTED_STRING;
+			}
+			else { (*token)->type = TPBRT_TOKEN_TYPE_SINGLE; }
+		return TPBRT_ERROR_NONE;
+	}
+
+	tpbrt_size_t tpbrt_token_size(const tpbrt_token_t* const token) { return token != TPBRT_NULL ? token->value.size : 0u; }
+
+	tpbrt_bool_t tpbrt_token_is_quoted_string(const tpbrt_token_t* const token) {
+		return token != TPBRT_NULL ? token->type == TPBRT_TOKEN_TYPE_QUOTED_STRING : TPBRT_FALSE;
+	}
+
+	tpbrt_bool_t tpbrt_token_is_directive(const tpbrt_token_t* const token) {
+		return token != TPBRT_NULL ? token->type == TPBRT_TOKEN_TYPE_DIRECTIVE : TPBRT_FALSE;
+	}
+
+	tpbrt_bool_t tpbrt_token_is_open_brace(const tpbrt_token_t* const token) {
+			if (token == TPBRT_NULL || token->type != TPBRT_TOKEN_TYPE_SINGLE) { return TPBRT_FALSE; }
+		return token->value.size == 1u && token->value.chars[0] == '{';
+	}
+
+	tpbrt_bool_t tpbrt_token_is_close_brace(const tpbrt_token_t* const token) {
+			if (token == TPBRT_NULL || token->type != TPBRT_TOKEN_TYPE_SINGLE) { return TPBRT_FALSE; }
+		return token->value.size == 1u && token->value.chars[0] == '}';
+	}
+
+	tpbrt_bool_t tpbrt_token_is_valid(const tpbrt_token_t* const token) {
+			if (token == TPBRT_NULL || token->value.size == 0u) { return TPBRT_FALSE; }
+
+			if (token->value.size >= 1u) {
+				const tpbrt_bool_t starts_with_quote = token->value.chars[0] == '\"';
+				const tpbrt_bool_t ends_with_quote	 = token->value.chars[token->value.size - 1] == '\"';
+
+					if (starts_with_quote || ends_with_quote) {
+							if (starts_with_quote != ends_with_quote) { return TPBRT_FALSE; }
+
+							if (token->value.size < 2u) { return TPBRT_FALSE; }
+					}
+
+					if (!starts_with_quote) {
+							for (tpbrt_size_t i = 0u; i < token->value.size; ++i) {
+									if (token->value.chars[i] == ' ') { return TPBRT_FALSE; }
+							}
+					}
+			}
+
+		return TPBRT_TRUE;
+	}
+
+	void tpbrt_free_token(tpbrt_token_t** const token) {
+			if (token == TPBRT_NULL || *token == TPBRT_NULL) { return; }
+
+		free(*token);
+		*token = TPBRT_NULL;
+	}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ tinypbrt/src/tinypbrt/detail/token_internal.h
@@ -0,0 +1,91 @@
+#pragma once
+#ifndef _TINYPBRT_TOKEN_INTERNAL_H_
+#define _TINYPBRT_TOKEN_INTERNAL_H_
+
+#include <tinypbrt/detail/common_internal.h>
+#include <tinypbrt/detail/error.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	typedef enum {
+		TPBRT_DIRECTIVE_IDENTITY			= 0,
+		TPBRT_DIRECTIVE_TRANSLATE			= 1,
+		TPBRT_DIRECTIVE_SCALE				= 2,
+		TPBRT_DIRECTIVE_ROTATE				= 3,
+		TPBRT_DIRECTIVE_LOOK_AT				= 4,
+		TPBRT_DIRECTIVE_COORDINATE_SYSTEM	= 5,
+		TPBRT_DIRECTIVE_COORD_SYS_TRANSFORM = 6,
+		TPBRT_DIRECTIVE_TRANSFORM			= 7,
+		TPBRT_DIRECTIVE_CONCAT_TRANSFORM	= 8,
+		TPBRT_DIRECTIVE_TRANSFORM_TIMES		= 9,
+		TPBRT_DIRECTIVE_ACTIVE_TRANSFORM	= 10,
+
+		TPBRT_DIRECTIVE_INCLUDE				= 11,
+		TPBRT_DIRECTIVE_IMPORT				= 12,
+
+		TPBRT_DIRECTIVE_OPTION				= 13,
+
+		TPBRT_DIRECTIVE_CAMERA				= 14,
+		TPBRT_DIRECTIVE_SAMPLER				= 15,
+		TPBRT_DIRECTIVE_COLOR_SPACE			= 16,
+		TPBRT_DIRECTIVE_FILM				= 17,
+		TPBRT_DIRECTIVE_PIXEL_FILTER		= 18,
+		TPBRT_DIRECTIVE_INTEGRATOR			= 19,
+		TPBRT_DIRECTIVE_ACCELERATOR			= 20,
+
+		TPBRT_DIRECTIVE_WORLD_BEGIN			= 21,
+
+		TPBRT_DIRECTIVE_ATTRIBUTE_BEGIN		= 22,
+		TPBRT_DIRECTIVE_ATTRIBUTE_END		= 23,
+		TPBRT_DIRECTIVE_ATTRIBUTE			= 24,
+
+		TPBRT_DIRECTIVE_REVERSE_ORIENTATION = 25,
+		TPBRT_DIRECTIVE_SHAPE				= 26,
+		TPBRT_DIRECTIVE_OBJECT_BEGIN		= 27,
+		TPBRT_DIRECTIVE_OBJECT_END			= 28,
+		TPBRT_DIRECTIVE_OBJECT_INSTANCE		= 29,
+
+		TPBRT_DIRECTIVE_LIGHT_SOURCE		= 30,
+		TPBRT_DIRECTIVE_AREA_LIGHT_SOURCE	= 31,
+
+		TPBRT_DIRECTIVE_MATERIAL			= 32,
+		TPBRT_DIRECTIVE_MAKE_NAMED_MATERIAL = 33,
+		TPBRT_DIRECTIVE_NAMED_MATERIAL		= 34,
+
+		TPBRT_DIRECTIVE_TEXTURE				= 35,
+
+		TPBRT_DIRECTIVE_MAKE_NAMED_MEDIUM	= 36,
+		TPBRT_DIRECTIVE_MEDIUM_INTERFACE	= 37,
+
+		TPBRT_DIRECTIVE_MAX_NUM				= 38,
+	} tpbrt_directive_t;
+
+	typedef enum {
+		TPBRT_TOKEN_TYPE_SINGLE		   = 0,
+		TPBRT_TOKEN_TYPE_QUOTED_STRING = 1,
+		TPBRT_TOKEN_TYPE_DIRECTIVE	   = 2,
+	} tpbrt_token_type_t;
+
+	typedef struct {
+		tpbrt_token_type_t type;
+		tpbrt_string_t value;
+		tpbrt_directive_t directive;
+	} tpbrt_token_t;
+
+	tpbrt_error_t tpbrt_create_token(const tpbrt_string_t* value, tpbrt_token_t** token);
+
+	tpbrt_size_t tpbrt_token_size(const tpbrt_token_t* token);
+	tpbrt_bool_t tpbrt_token_is_quoted_string(const tpbrt_token_t* token);
+	tpbrt_bool_t tpbrt_token_is_directive(const tpbrt_token_t* token);
+	tpbrt_bool_t tpbrt_token_is_open_brace(const tpbrt_token_t* token);
+	tpbrt_bool_t tpbrt_token_is_close_brace(const tpbrt_token_t* token);
+	tpbrt_bool_t tpbrt_token_is_valid(const tpbrt_token_t* token);
+
+	void tpbrt_free_token(tpbrt_token_t** token);
+
+#ifdef __cplusplus
+}
+#endif
+#endif // _TINYPBRT_TOKEN_INTERNAL_H_
